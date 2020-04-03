f1bbb70ce708637108b293d3ba0c0c169fb8d710
DominicWuest <dominic.wuest@hotmail.com>
2020-04-03 03:04:16 +0200

    Improved looks and syncing

--- app/src/main/java/ch/dyn/mal/companionwatch/WatchActivity.java
+++ app/src/main/java/ch/dyn/mal/companionwatch/WatchActivity.java
@@ -47,20 +47,32 @@ public class WatchActivity extends YouTubeBaseActivity {
     YouTubePlayer.OnInitializedListener onInitializedListener;
 
     // A boolean indicating whether the last state change has been caused by the user or by a socket event
-    boolean externalChange = false;
+    boolean externalChange;
 
     // A boolean indicating whether the user has newly joined the room and plays the video for the first time
-    boolean firstPlay = true;
+    boolean firstPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch);
 
+        externalChange = false;
+        firstPlay = true;
+
         // Objects displayed on screen
         searchInput = findViewById(R.id.editText);
         searchButton = findViewById(R.id.button);
         visibilityToggle = findViewById(R.id.visibilityToggle);
+
+        // Initializing the Web-Socket
+        try {
+            IO.Options opt = new IO.Options();
+            opt.query = "ns=".concat(namespace);
+            mSocket = IO.socket("https://mal.dyn.ch/watch", opt);
+        } catch (URISyntaxException e) {}
+
+        mSocket.connect();
 
         // Initializing the youtube player
         youTubePlayerView = findViewById(R.id.youtube_player);
@@ -77,14 +89,19 @@ public class WatchActivity extends YouTubeBaseActivity {
                     public void onPlaying() {
                         // Request a time sync if the user newly joined the room and the video starts playing
                         if (firstPlay) {
+                            youTubePlayer.pause();
+                            externalChange = true;
+                            mSocket.emit("requestStateSync");
                             mSocket.emit("requestTimeSync");
                             firstPlay = false;
-                        }
-                        // Don't emit an event if the state change was caused by another client
-                        if (externalChange) externalChange = false;
-                        else {
-                            double millis = youTubePlayer.getCurrentTimeMillis() / 1000.0;
-                            mSocket.emit("stateChange", 1, millis);
+                            return;
+                        } else {
+                            // Don't emit an event if the state change was caused by another client
+                            if (externalChange) externalChange = false;
+                            else {
+                                double millis = youTubePlayer.getCurrentTimeMillis() / 1000.0;
+                                mSocket.emit("stateChange", 1, millis);
+                            }
                         }
                     }
 
@@ -119,6 +136,93 @@ public class WatchActivity extends YouTubeBaseActivity {
                     }
                 });
 
+                // ----------------- Start of Emitter listeners declaration -----------------
+
+                onStateChange = new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                int data = (int) args[0];
+                                switch (data) {
+                                    case 1: youTubePlayer.play(); externalChange = true; break;
+                                    case 2: youTubePlayer.pause(); externalChange = true; break;
+                                }
+                            }
+                        });
+                    }
+                };
+
+                onTimeChange = new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                externalChange = true;
+                                double seconds = (double) args[0];
+                                int millis = (int) (seconds * 1000.0);
+                                youTubePlayer.seekToMillis(millis);
+                            }
+                        });
+                    }
+                };
+
+                onVideoChange = new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                String videoId = (String) args[0];
+                                // Only cue the video if it's the first video played by the user
+                                youTubePlayer.loadVideo(videoId);
+
+                            }
+                        });
+                    }
+                };
+
+                onVisibilityChange = new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                boolean visibility = (boolean) args[0];
+                                visibilityToggle.setChecked(visibility);
+                            }
+                        });
+                    }
+                };
+
+                onSearchResults = new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        });
+                    }
+                };
+
+                // ------------------ End of Emitter listeners declaration ------------------
+
+                // Event listeners for the Web-Socket
+
+                mSocket.on("stateChange", onStateChange);
+                mSocket.on("timeChange", onTimeChange);
+                mSocket.on("videoChange", onVideoChange);
+                mSocket.on("visibilityChange", onVisibilityChange);
+                mSocket.on("searchResults", onSearchResults);
+
+                // Emit events to sync the current players state with other watchers
+                mSocket.emit("requestVideoSync");
+                mSocket.emit("requestVisibilitySync");
+                mSocket.emit("requestStateSync");
             }
 
             @Override
@@ -129,101 +233,6 @@ public class WatchActivity extends YouTubeBaseActivity {
 
         // Initialize the youtube video player
         youTubePlayerView.initialize(DeveloperVariables.API_KEY, onInitializedListener);
-
-        // Initializing the Web-Socket
-        try {
-            IO.Options opt = new IO.Options();
-            opt.query = "ns=".concat(namespace);
-            mSocket = IO.socket("https://mal.dyn.ch/watch", opt);
-        } catch (URISyntaxException e) {}
-
-        mSocket.connect();
-
-        // ----------------- Start of Emitter listeners declaration -----------------
-
-        onStateChange = new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        int data = (int) args[0];
-                        switch (data) {
-                            case 1: youTubePlayer.play(); externalChange = true; break;
-                            case 2: youTubePlayer.pause(); externalChange = true; break;
-                        }
-                    }
-                });
-            }
-        };
-
-        onTimeChange = new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        externalChange = true;
-                        double seconds = (double) args[0];
-                        int millis = (int) (seconds * 1000.0);
-                        youTubePlayer.seekToMillis(millis);
-                    }
-                });
-            }
-        };
-
-        onVideoChange = new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String videoId = (String) args[0];
-                        youTubePlayer.loadVideo(videoId);
-                    }
-                });
-            }
-        };
-
-        onVisibilityChange = new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        boolean visibility = (boolean) args[0];
-                        visibilityToggle.setChecked(visibility);
-                    }
-                });
-            }
-        };
-
-        onSearchResults = new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                    }
-                });
-            }
-        };
-
-        // ------------------ End of Emitter listeners declaration ------------------
-
-        // Event listeners for the Web-Socket
-
-        mSocket.on("stateChange", onStateChange);
-        mSocket.on("timeChange", onTimeChange);
-        mSocket.on("videoChange", onVideoChange);
-        mSocket.on("visibilityChange", onVisibilityChange);
-        mSocket.on("searchResults", onSearchResults);
-
-        // Emit events to sync the current players state with other watchers
-        mSocket.emit("requestVideoSync");
-        mSocket.emit("requestVisibilitySync");
-        mSocket.emit("requestStateSync");
 
         // Perform a search if the button is pressed
         searchButton.setOnClickListener(new View.OnClickListener() {
@@ -255,9 +264,23 @@ public class WatchActivity extends YouTubeBaseActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSocket.disconnect();
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
 
-        mSocket.disconnect();
+        youTubePlayer.release();
+        youTubePlayer = null;
+
+        mSocket.off("stateChange", onStateChange);
+        mSocket.off("timeChange", onTimeChange);
+        mSocket.off("videoChange", onVideoChange);
+        mSocket.off("visibilityChange", onVisibilityChange);
+        mSocket.off("searchResults", onSearchResults);
     }
 }
